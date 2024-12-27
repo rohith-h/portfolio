c212f53ae367b61f71ab9ce19231d3c226f373f1
rohith-h <75358685+rohith-h@users.noreply.github.com>
2024-12-27 16:18:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+const body = document.body;
+
+const displayList = () => {
+  const navUl = document.querySelector('.nav__list');
+
+  if (btnHamburger.classList.contains('fa-bars')) {
+    btnHamburger.classList.remove('fa-bars');
+    btnHamburger.classList.add('fa-times');
+    navUl.classList.add('display-nav-list');
+  } else {
+    btnHamburger.classList.remove('fa-times');
+    btnHamburger.classList.add('fa-bars');
+    navUl.classList.remove('display-nav-list');
+  }
+};
+
+btnHamburger.addEventListener('click', displayList);
